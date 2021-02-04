0fe1c0559c15ac3bfb2ae71b3bcfc7d3dd97383b
apmarshall <apmarshall@soren.tech>
2021-02-04 17:03:16 -0500

    Changing URLs to use archives instead of home page

--- review_spiders/spiders/brookings_spider.py
+++ review_spiders/spiders/brookings_spider.py
@@ -4,7 +4,17 @@ class BrookingsSpider(scrapy.Spider):
     name = "Brookings Institution"
 
     def start_urls = [
-            'https://www.brookings.edu/'
+            'https://www.brookings.edu/search/?post_type=post&orderby=date',
+            'https://www.brookings.edu/search/?post_type=article&orderby=date',
+            'https://www.brookings.edu/search/?post_type=essay&orderby=date',
+            'https://www.brookings.edu/search/?post_type=interactive&orderby=date',
+            'https://www.brookings.edu/search/?post_type=on-the-record&orderby=date',
+            'https://www.brookings.edu/search/?post_type=opinion&orderby=date',
+            'https://www.brookings.edu/search/?post_type=product&orderby=date',
+            'https://www.brookings.edu/search/?post_type=research&orderby=date',
+            'https://www.brookings.edu/search/?post_type=techstream&orderby=date',
+            'https://www.brookings.edu/search/?post_type=testimony&orderby=date',
+            'https://www.brookings.edu/search/?post_type=voter-vital&orderby=date'
         ]
 
     def parse(self, response):
