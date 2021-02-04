e009f2f3b56ca40d9e57946115dd85f5cb2048ae
apmarshall <apmarshall@soren.tech>
2021-02-04 17:05:55 -0500

    Drop the label line, it's not especially helpful for us

--- review_spiders/spiders/brookings_spider.py
+++ review_spiders/spiders/brookings_spider.py
@@ -23,7 +23,6 @@ class BrookingsSpider(scrapy.Spider):
                 'title': article.css('h4.title::text').get(),
                 'author': article.css('div.authors::text').getall(),
                 'date': article.css('time::text').get(),
-                'topics': article.css('a.label::text').getall(),
                 'url': article.css('h4 + a').get(),
                 'content': /* follow-link here to get text */
             }
