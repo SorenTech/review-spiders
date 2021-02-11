99a57b992fcf83db64f7bb4fff145b077213ab60
apmarshall <apmarshall@soren.tech>
2021-02-11 16:12:45 -0500

    Defining our custom item fields

--- review_spiders/items.py
+++ review_spiders/items.py
@@ -4,9 +4,16 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.item import Item, Field
 
-
-class ReviewSpidersItem(scrapy.Item):
+class ReviewSpidersItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = Field()
+    authors = Field()
+    date = Field()
+    topic = Field()
+    tags = Field()
+    content = Field()
+    copyright = Field()
+
