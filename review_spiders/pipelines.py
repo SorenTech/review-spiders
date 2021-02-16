fb0ef67dda240330796c2ae3bb7b945b9b6bff4a
apmarshall <apmarshall@soren.tech>
2021-02-16 14:55:36 -0500

    Test pipeline to help us debug output of spiders

--- review_spiders/pipelines.py
+++ review_spiders/pipelines.py
@@ -6,8 +6,22 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+from scrapy.exporters import JsonLinesItemExporter
 
 class ReviewSpidersPipeline:
+    def close_spider(self, spider):
+        exporter.finish_exporting()
+
+    def _exporter_for_item(self, item):
+        adapter = ItemAdapter(item)
+        f = open('~/Code/soren-review/test-data.json')
+        exporter = JsonLinesItemExporter(f)
+        exporter.start_exporting()
+
     def process_item(self, item, spider):
+        exporter = self._exporter_for_item(item)
+        exporter.export_item(item)
         return item
+
+
+
